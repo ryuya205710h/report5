1493a61f3c45a860dfb090f2383de51e09010ee3
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2021-12-19 16:37:12 +0900

    report5

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import java.lang.NullPointerException;
 public class Main{
     public static void main(String[] arg) {
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
